02ff09b6f389661da7f47465c62616d006f080c4
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2026-05-27 12:45:15 +0200

    MKL stage works for both ubuntu and rhel

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -1,11 +1,11 @@
 FROM base 
 ARG CMAKE_URL
-ARG CMAKE_URL_CHECKSUM
+ARG CMAKE_URL_CHECKSUM_SHA256
 
 RUN apt-get update -y \
  && apt-get install -y --no-install-recommends \
       aria2 ca-certificates \
- && aria2c --checksum="${CMAKE_URL_CHECKSUM}" -d /tmp -o cmake-install.x "${CMAKE_URL}" \
+ && aria2c --checksum=sha-256="${CMAKE_URL_CHECKSUM_SHA256}" -d /tmp -o cmake-install.x "${CMAKE_URL}" \
  && chmod +x /tmp/cmake-install.x \
  && mkdir -p /opt/cmake \
  && /tmp/cmake-install.x --skip-license --prefix=/opt/cmake \
