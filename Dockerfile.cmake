ddf030ade7d60c9bdb7cb772f88dd775371599fd
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2026-05-27 14:14:47 +0200

    Reuse downloaded artifacts

--- Dockerfile.cmake
+++ Dockerfile.cmake
@@ -1,11 +1,25 @@
-FROM base
+# ==========================================
+# Downloader stage
+# Keep builder and downloader separated so
+# different builders can reuse the same
+# downloaded artifact.
+# ==========================================
+FROM base AS cmake-downloader
 
 ARG CMAKE_URL
 ARG CMAKE_URL_CHECKSUM_SHA256
 
 RUN wget -O /tmp/cmake-install.x -q "${CMAKE_URL}" \
  && echo "${CMAKE_URL_CHECKSUM_SHA256} /tmp/cmake-install.x" | sha256sum --check - \
- && chmod +x /tmp/cmake-install.x \
- && mkdir -p /opt/cmake \
+ && chmod +x /tmp/cmake-install.x
+
+# ==========================================
+# Builder stage
+# ==========================================
+FROM base
+
+COPY --from=cmake-downloader /tmp/cmake-install.x /tmp/cmake-install.x
+
+RUN mkdir -p /opt/cmake \
  && /tmp/cmake-install.x --prefix=/opt/cmake --skip-license \
  && rm -rf /tmp/*
